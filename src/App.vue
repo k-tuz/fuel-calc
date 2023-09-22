a89d8a3e5dfad01d72cca826d02b102f3205c157
k_tuz <Kostiantyn Tuz>
2023-09-22 18:48:28 +0300

    test build

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <script setup lang="ts">
-import {useStore} from '@/stores/store'
+/* import {useStore} from '@/stores/store'
 import { ModalsContainer } from 'vue-final-modal'
 import Header from "@/components/Header.vue"
 import AppInit from "@/components/AppInit.vue"
 import PressStart from "@/components/PressStart.vue"
 
-const store = useStore()
+const store = useStore() */
 
 </script>
 
 <template>
-  <Header />
+  <h1>test</h1>
+  <!-- <Header />
 
   <main class="main">
     <component :is="!store.isInit ? PressStart : AppInit"></component>
   </main>
 
-  <ModalsContainer />
+  <ModalsContainer /> -->
 </template>
 
 <style>
