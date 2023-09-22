aec753d820304754686d08b4cf24946813f1945c
k_tuz <Kostiantyn Tuz>
2023-09-22 19:42:11 +0300

    test build

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import 'normalize.css'
-import 'vue-final-modal/style.css'
-import '@/assets/main.css'
+//import 'vue-final-modal/style.css'
+//import '@/assets/main.css'
 
 import { createApp } from 'vue'
-import { createVfm } from 'vue-final-modal'
-import { createPinia } from 'pinia'
+//import { createVfm } from 'vue-final-modal'
+//import { createPinia } from 'pinia'
 import App from '@/App.vue'
 
 const app = createApp(App)
-const vfm = createVfm()
+//const vfm = createVfm()
 
-app.use(createPinia())
-app.use(vfm)
+//app.use(createPinia())
+//app.use(vfm)
 
 app.mount('#app')
